e6c646f1269c4a16e4e81ad67b215edf0453d14a
folkaras <efolkaras@gmail.com>
2019-12-06 22:59:15 +0300

    created modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <section class="modal-btns">
+      <button class="modal-btns__btn" @click="simpleModal.isOpen = true">Open model window</button>
+			<button class="modal-btns__btn" @click="formModal.isOpen = true">Open model window</button>
+    </section>
+    <transition name="modal">
+      <Modal v-if="simpleModal.isOpen" @close="simpleModal.isOpen" title="New Modal">
+        <div slot="body">
+					<p>Simplest modal window</p>
+          <button class="modal-btns__btn" @click.prevent="isModelOpen = false">Close model window</button>
+        </div>
+      </Modal>
+    </transition>
+		<transition name="modal">
+			<form slot="body">
+
+			</form>
+		</transition>
   </div>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
 export default {
   name: "app",
-  components: {}
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      simpleModal: {
+				isOpen: false
+			},
+			formModal: {
+				isOpen: false,
+				formData: {
+
+				}
+			}
+    };
+  },
+  methods: {
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="modal">
+      <div class="modal__backdrop" @click="modalClose"></div>
+      <div class="modal__window">
+        <h3 class="modal__title">{{ title }}</h3>
+        <div class="modal__body">
+          <slot name="body"></slot>
+        </div>
+        <button class="modal__close" type="button" @click="modalClose">x</button>
+      </div>
+    </div>
+
+</template>
+<script>
+export default {
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Modal Window!"
+    }
+  },
+  mounted() {
+    document.body.addEventListener("keyup", e => {
+      if (e.keyCode === 27) {
+        this.$emit("close");
+      }
+    });
+  },
+  methods: {
+    modalClose() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
