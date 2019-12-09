68f6b73fb08ca8e2e788ba46dc9a96c92ba39cf8
folkaras <efolkaras@gmail.com>
2019-12-09 10:17:35 +0300

    add new modal

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,33 @@
   <div id="app">
     <section class="modal-btns">
       <button class="modal-btns__btn" @click="simpleModal.isOpen = true">Open model window</button>
-			<button class="modal-btns__btn" @click="formModal.isOpen = true">Open model window</button>
+      <button class="modal-btns__btn" @click="formModal.isOpen = true">Open model window</button>
     </section>
     <transition name="modal">
-      <Modal v-if="simpleModal.isOpen" @close="simpleModal.isOpen" title="New Modal">
+      <Modal v-if="simpleModal.isOpen" @close="simpleModal.isOpen = false" title="New Modal">
         <div slot="body">
-					<p>Simplest modal window</p>
-          <button class="modal-btns__btn" @click.prevent="isModelOpen = false">Close model window</button>
+          <p>Simplest modal window</p>
+          <button
+            class="modal-btns__btn"
+            @click.prevent="simpleModal.isOpen = false"
+          >Close model window</button>
         </div>
       </Modal>
     </transition>
-		<transition name="modal">
-			<form slot="body">
-
-			</form>
-		</transition>
+    <transition name="modal">
+      <Modal v-if="formModal.isOpen" @close="modalClose(formModal)">
+        <form slot="body" @submit.prevent="modalClose(formModal)">
+          <label>
+            Name:
+            <input type="text" v-model="formModal.name" />
+          </label>
+          <label>
+            email:
+            <input type="text" v-model="formModal.email" />
+          </label>
+        </form>
+      </Modal>
+    </transition>
   </div>
 </template>
 
@@ -30,17 +42,24 @@ export default {
   data() {
     return {
       simpleModal: {
-				isOpen: false
-			},
-			formModal: {
-				isOpen: false,
-				formData: {
-
-				}
-			}
+        isOpen: false
+      },
+      formModal: {
+        isOpen: false,
+				name: "",
+				email: ""
+      }
     };
   },
   methods: {
+    modalClose(name) {
+      for (let item in name) {
+        if (typeof name[item] === "string") {
+          name[item] = "";
+        }
+      }
+      name.isOpen = false;
+    }
   }
 };
 </script>
